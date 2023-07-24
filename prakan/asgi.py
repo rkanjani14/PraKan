aed0d6b493aea326f0c262a09b97257cd153d63f
Rakesh Anjani <rkanjani14@gmail.com>
2023-07-24 22:01:35 +0530

    import django in asgi file for daphne server setting

--- prakan/asgi.py
+++ prakan/asgi.py
@@ -1,11 +1,12 @@
 import os
+import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from chatapp.routing import websocket_urlpatterns
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'prakan.settings')
 asgi_application = get_asgi_application()
-
+django.setup()
 application = ProtocolTypeRouter({
     'http':asgi_application,
     'websocket': AuthMiddlewareStack(
