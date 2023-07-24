31ea8968d5ce43ef2cc27d5db52f3c12dac50321
Rakesh Anjani <rkanjani14@gmail.com>
2023-07-24 22:54:04 +0530

    asgi changes for environment variable pattern

--- prakan/asgi.py
+++ prakan/asgi.py
@@ -1,12 +1,14 @@
 import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'prakan.settings')
 import django
+django.setup()
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from chatapp.routing import websocket_urlpatterns
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'prakan.settings')
+
 asgi_application = get_asgi_application()
-django.setup()
+
 application = ProtocolTypeRouter({
     'http':asgi_application,
     'websocket': AuthMiddlewareStack(
